963948cdd0a5e00b0c9f9a3e8d9ba37ae080afd8
hamtorililil <hojin8876@naver.com>
2022-04-07 15:05:13 +0900

    ADD: Dice.java file

---
+++ pj1/Dice.java
@@ -0,0 +1,29 @@
+public class Dice {
+    private int value;
+
+    public Dice(){
+        value=0;
+
+    }
+
+    public void Roll(){
+        value = (int)(math.random()*6)+1;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public static void main(String[] args) {
+        Dice dite1, dite2;
+        int rollcounter;
+
+        dite1 = new Dice();
+        dite2 = new Dice();
+    }
+
+}
