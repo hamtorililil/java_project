e2ce4150929ea411f03acd40ab9b6a0281ca9198
hamtorililil <hojin8876@naver.com>
2022-05-09 13:30:00 +0900

    commit

---
+++ school_jp/src/제5장클래스와객체2/MyMath.java
@@ -0,0 +1,11 @@
+package 제5장클래스와객체2;
+
+public class MyMath {
+    public static int abs(int x){ return x>0?x:-x;}
+    public static int power(int base, int exponent){
+        int result = 1;
+        for (int i = 1; i <= exponent; i++)
+            result *= base;
+        return result;
+    }
+}
