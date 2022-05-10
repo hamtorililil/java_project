90ece7967c50fd79f465d59b297acaf3a390a635
hamtorililil <hojin8876@naver.com>
2022-05-10 17:02:06 +0900

    commit

---
+++ school_jp/src/제6장상속/DynamicCallTest.java
@@ -0,0 +1,28 @@
+package 제6장상속;
+
+class Animal2 {
+    void speak() {
+        System.out.println("Animal 클래스의 sound()");
+    }
+}
+class Dog2 extends Animal2 {
+    void speak() {
+        System.out.println("멍멍");
+    }
+}
+class Cat2 extends Animal2 {
+    void speak() {
+        System.out.println("야옹");
+    }
+}
+
+
+public class DynamicCallTest {
+    public static void main(String[] args) {
+        Animal2 a1 = new Dog2();
+        Animal2 a2 = new Cat2();
+
+        a1.speak(); // 어떤 sound()가 호출될 것인지는 실행 시간에 참조되는
+        a2.speak(); // 객체의 타입에 따라서 결정된다.
+    }
+}
---
+++ school_jp/src/제6장상속/EmpLayeeTest.java
@@ -0,0 +1,34 @@
+package 제6장상속;
+
+class Date{
+    private int year, month, date;
+
+    public Date(int year, int month, int date){
+        this.year = year;
+        this.month = month;
+        this.date = date;
+    }
+    @Override
+    public String toString(){
+        return "Date [year=" + year + ", month= " + month + ", date=" + date + "]";
+    }
+}
+class Employee3{
+    private String name;
+    private Date birthDate;
+    public Employee3(String name, Date birthDate){
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+    @Override
+    public String toString(){
+        return "Employee [name=" + name + ", birthDate=" + birthDate + "]";
+    }
+}
+public class EmpLayeeTest {
+    public static void main(String[] args) {
+        Date birth = new Date(1990, 1, 1);
+        Employee3 employee3 = new Employee3("홍길동", birth);
+        System.out.println(employee3);
+    }
+}
