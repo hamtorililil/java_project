221420409a542f02723b283748042df0a975d7d5
hamtorililil <hojin8876@naver.com>
2022-05-10 12:44:33 +0900

    commit

---
+++ school_jp/src/제5장클래스와객체2/SingleTest.java
@@ -0,0 +1,18 @@
+package 제5장클래스와객체2;
+class Single{
+    private static Single instance = new Single();
+    private Single(){ }//전용 생성자
+
+    public static Single getInstance(){
+        return instance;
+    }
+}
+public class SingleTest {
+    public static void main(String[] args) {
+        Single obj1 = Single.getInstance();
+        Single obj2 = Single.getInstance();
+        System.out.println(obj1);
+        System.out.println(obj2);
+    }
+
+}
