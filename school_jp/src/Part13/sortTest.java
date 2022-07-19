406dba9445565860420c678f01fd82957b6e23ec
hamtorililil <hojin8876@naver.com>
2022-07-19 11:02:51 +0900

    commit

---
+++ school_jp/src/Part13/sortTest.java
@@ -0,0 +1,39 @@
+package Part13;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class sortTest {
+    class Student implements Comparable<Student> {
+        int number;
+        String name;
+
+        public Student(int number, String name) {
+            this.number = number;
+            this.name = name;
+        }
+
+        public String toString() {
+            return name;
+        }
+
+        public int compareTo(Student s) {
+            return s.number - number;
+        }
+    }
+
+    public class SortTest {
+        public void main(String[] args) {
+            Student array[] = {
+                    new Student(2, "김철수"),
+                    new Student(3, "이철수"),
+                    new Student(1, "박철수"),
+            };
+            List<Student> list = Arrays.asList(array);
+            Collections.sort(list);
+            System.out.println(list);
+            SortTest f = new SortTest();
+        }
+    }
+}
